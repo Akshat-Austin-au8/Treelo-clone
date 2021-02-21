758cadfbeaeb2c17bd26136ba2393196ba737282
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-02-21 14:34:58 +0530

    treelo changes

--- src/components/TrelloBoard.js
+++ src/components/TrelloBoard.js
@@ -1,32 +1,33 @@
-import React, { PureComponent } from "react";
-import TrelloList from "./TrelloList";
-import { connect } from "react-redux";
-import TrelloCreate from "./TrelloCreate";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
-import styled from "styled-components";
-import { sort, setActiveBoard } from "../actions";
-import { Link } from "react-router-dom";
+import React, { PureComponent } from 'react'
+import TrelloList from './TrelloList'
+import { connect } from 'react-redux'
+import TrelloCreate from './TrelloCreate'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
+import styled from 'styled-components'
+import { sort, setActiveBoard } from '../actions'
+import { Link } from 'react-router-dom'
+import './styles.css'
 
 const ListsContainer = styled.div`
   display: flex;
   flex-direction: row;
-`;
+`
 
 // TODO: Fix performance issue
 
 class TrelloBoard extends PureComponent {
   componentDidMount() {
     // set active trello board here
-    const { boardID } = this.props.match.params;
+    const { boardID } = this.props.match.params
 
-    this.props.dispatch(setActiveBoard(boardID));
+    this.props.dispatch(setActiveBoard(boardID))
   }
 
-  onDragEnd = result => {
-    const { destination, source, draggableId, type } = result;
+  onDragEnd = (result) => {
+    const { destination, source, draggableId, type } = result
 
     if (!destination) {
-      return;
+      return
     }
 
     this.props.dispatch(
@@ -38,32 +39,34 @@ class TrelloBoard extends PureComponent {
         draggableId,
         type
       )
-    );
-  };
+    )
+  }
 
   render() {
-    const { lists, cards, match, boards } = this.props;
-    const { boardID } = match.params;
-    const board = boards[boardID];
+    const { lists, cards, match, boards } = this.props
+    const { boardID } = match.params
+    const board = boards[boardID]
     if (!board) {
-      return <p>Board not found</p>;
+      return <p>Board not found</p>
     }
-    const listOrder = board.lists;
+    const listOrder = board.lists
 
     return (
       <DragDropContext onDragEnd={this.onDragEnd}>
-        <Link to="/">Go Back</Link>
-        <h2>{board.title}</h2>
-        <Droppable droppableId="all-lists" direction="horizontal" type="list">
-          {provided => (
+        <h2 className='board'>{board.title}</h2>
+        <Link className='go' to='/'>
+          Go Back
+        </Link>
+        <Droppable droppableId='all-lists' direction='horizontal' type='list'>
+          {(provided) => (
             <ListsContainer
               {...provided.droppableProps}
               ref={provided.innerRef}
             >
               {listOrder.map((listID, index) => {
-                const list = lists[listID];
+                const list = lists[listID]
                 if (list) {
-                  const listCards = list.cards.map(cardID => cards[cardID]);
+                  const listCards = list.cards.map((cardID) => cards[cardID])
 
                   return (
                     <TrelloList
@@ -73,7 +76,7 @@ class TrelloBoard extends PureComponent {
                       cards={listCards}
                       index={index}
                     />
-                  );
+                  )
                 }
               })}
               {provided.placeholder}
@@ -82,14 +85,14 @@ class TrelloBoard extends PureComponent {
           )}
         </Droppable>
       </DragDropContext>
-    );
+    )
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   lists: state.lists,
   cards: state.cards,
-  boards: state.boards
-});
+  boards: state.boards,
+})
 
-export default connect(mapStateToProps)(TrelloBoard);
+export default connect(mapStateToProps)(TrelloBoard)
